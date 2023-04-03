1e500bf4cb1c59d4132b2cd7b28589780b91c8ed
Onegen <84882649+Onegenimasu@users.noreply.github.com>
2023-04-04 00:40:21 +0200

    refactor(interpret): separate file reading to its own function

--- interpret.py
+++ interpret.py
@@ -12,25 +12,28 @@ from lib_interpret.ippc_interpreter import Interpreter
 from lib_interpret.ippc_idata import IEXCEPTIONC, RETCODE
 
 """
+------------------------------------------------------------------------------
 Pomocné funkcie
     @func print_help(): vypíše nápovedu na stdout a ukončí program
     @func parse_args(): spracuje argumenty programu
+    @func read_file_content(str): načíta obsah súboru
     @func throw_err(str, str): vypíše chybovú hlášku na stderr a ukončí program
+------------------------------------------------------------------------------
 """
 
 
 def print_help():
-    print("Usage: python3[.10] interpret.py [OPTIONS] <in out>")
-    print("")
-    print(" Interprets the XML representation of a IPPcode23 program.")
-    print("")
-    print("  --help              Prints this help message and exits.")
-    print("  --source=<file>     Specifies the source file to be interpreted.")
-    print("  --input=<file>      Specifies the input file to be used.")
-    print("")
-    print("   At least one of the options above must be specified.")
-    print("   Aside from --help, the unspecified option of the two")
-    print("   will be expected on standard input.")
+    help_msg = (
+        "Usage: python3[.10] interpret.py [OPTIONS] <in out>\n\n"
+        " Interprets the XML representation of a IPPcode23 program.\n\n"
+        "  --help              Prints this help message and exits.\n"
+        "  --source=<file>     Specifies the source file to be interpreted.\n"
+        "  --input=<file>      Specifies the input file to be used.\n\n"
+        "   At least one of the options above must be specified.\n"
+        "   Aside from --help, the unspecified option of the two\n"
+        "   will be expected on standard input."
+    )
+    print(help_msg)
     sys.exit(RETCODE.get("OK"))
 
 
@@ -52,9 +55,20 @@ def parse_args():
             arguments["input"] = arg
 
     if arguments["source"] is None and arguments["input"] is None:
-        throw_err("EPARAM", "--source or --input required", arguments)
+        throw_err("EPARAM", "--source or --input required")
 
     return arguments
+
+
+def read_file_content(file_path):
+    if file_path is None:
+        return sys.stdin.read()
+    else:
+        try:
+            with open(file_path, "r") as f:
+                return f.read()
+        except FileNotFoundError as error:
+            throw_err("ENOENT", str(error))
 
 
 def throw_err(ecode, msg, instr=None):
@@ -63,56 +77,47 @@ def throw_err(ecode, msg, instr=None):
     instr_label = "\033[35;49minstr\033[0m"
 
     err_str = f"{err_prefix} {code_label} {ecode}"
-
     if instr is not None:
         err_str += f"\n{err_prefix} {instr_label} {instr}"
-
     err_str += f"\n{err_prefix} {msg}"
-    print(err_str, file=sys.stderr)
 
+    print(err_str, file=sys.stderr)
     sys.exit(RETCODE.get(ecode))
 
 
 """
+------------------------------------------------------------------------------
 Hlavná časť programu
+------------------------------------------------------------------------------
+
 """
 
 
 def main():
     arguments = parse_args()
+    source_cont = read_file_content(arguments["source"])
+    input_cont = read_file_content(arguments["input"])
 
     try:
-        arguments["source"] = (
-            sys.stdin.read()
-            if arguments["source"] is None
-            else open(arguments["source"], "r").read()
-        )
-        arguments["input"] = (
-            sys.stdin.read()
-            if arguments["input"] is None
-            else open(arguments["input"], "r").read()
-        )
-    except FileNotFoundError as error:
-        throw_err("ENOENT", str(error))
-
-    try:
-        interpret = Interpreter(arguments.get("source"), arguments.get("input"))
+        interpret = Interpreter(source_cont, input_cont)
     except ET.ParseError as error:
         throw_err("EXML", str(error))
         sys.exit(RETCODE.get("EXML"))
     except Exception as error:  # skipcq: PYL-W0703 - catch all exceptions
-        throw_err("ESTRUC", error.args[0])
+        throw_err("ESTRUC", str(error))
         sys.exit(RETCODE.get("ESTRUC"))
 
-    returncode = -1
-    next_instruction = interpret.peek_instruction()
-    while next_instruction is not None:
+    returncode = 0
+    while True:
+        next_instruction = interpret.peek_instruction()
+        if next_instruction is None:
+            break
+
         try:
             returncode = interpret.execute_next()
-            next_instruction = interpret.peek_instruction()
         except Exception as error:  # skipcq: PYL-W0703 - catch all exceptions
             error_code = IEXCEPTIONC.get(type(error), "EINT")
-            throw_err(error_code, error.args[0], next_instruction)
+            throw_err(error_code, str(error), next_instruction)
 
     # DEBUG OUTPUT
     print("\n" + repr(interpret))
